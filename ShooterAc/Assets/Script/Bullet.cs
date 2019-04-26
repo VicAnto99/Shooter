ffd5ba08f8c925d0af82434df44c6c2d9594a806
VicAnto99 <A01339529@itesm.mx>
2019-04-26 18:55:34 -0500

    Subir

---
+++ ShooterAc/Assets/Script/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody rig;
+    public float speed;
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        rig.velocity = transform.right * speed;
+    }
+}
---
+++ ShooterAc/Assets/Script/MovePlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Variables
+{
+    public float xMin, xMax, zMin, zMax;
+}
+public class MovePlayer : MonoBehaviour
+{
+    [Header("Movimiento")]
+    public Variables var;
+    private Rigidbody rig;
+    public float speed;
+    [Header("Disparo")]
+    public GameObject shot;
+    public Transform shoot;
+    public float firerate;
+    private float nextfire;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+
+    }
+    void Update()
+    {
+        float MoveHo = Input.GetAxis("Horizontal");
+        float MoveVe = Input.GetAxis("Vertical");
+        Vector3 mov = new Vector3(MoveHo, MoveVe, 0f);
+        rig.velocity = mov * speed;
+        rig.position = new Vector3(Mathf.Clamp(rig.position.x, var.xMin, var.xMax), Mathf.Clamp(rig.position.y, var.zMin, var.zMax), 0f);
+        rig.rotation = Quaternion.Euler(4.205f, 0.168f, 88.73901f);
+        if (Input.GetButton("Fire1") && Time.time >  nextfire)
+        {
+            nextfire = Time.time + firerate;
+            Instantiate(shot, shoot.position, Quaternion.identity);
+        }
+    }
+}
